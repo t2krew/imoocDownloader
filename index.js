0003568352ec3bbd29925d22b09dced0e4d101cf
Krew <wanhao.huang@outlook.com>
2016-01-10 16:28:13 +0800

    update

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ let config = require('./config');
 let Crawler = require('./lib/main');
 
 let worker = new Crawler({
-	videoDir : './video',
-    target : [552,556,21,441,11],
     timeout : 4,
-    singleRule : ''
+	videoDir : './video',
+    target : [552,556,21,441,11]
 });
 
 
