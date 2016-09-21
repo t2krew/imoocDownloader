82c2bd4a0c9d2e8afbc12d3c10fe70966c09b408
Krew <wanhao.huang@outlook.com>
2016-09-22 00:52:32 +0800

    configure

---
+++ conf/config.default.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+module.exports = {
+  videoDir : './video',
+  targets : [317,348]
+}
---
+++ conf/config.local.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+
+  URI : {
+    COURSE_VIEW : `http://www.imooc.com/view/`,
+    SEARCH_COURSE : `http://www.imooc.com/search/course?words=`,
+    SEARCH_LESSON : `http://www.imooc.com/learn/`
+  }
+
+}
+
